5637507fd5c40ebc99e3f571a6f8ed37452fd734
BrenieEmnace <emnacebrenie@gmail.com>
2021-11-21 00:49:43 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('base')
+@section('content')
+
+<div class="text-center">
+    <h1>Merchandises Database</h1>
+</div>
+<div class="container-fluid">
+    <div class="d-flex flex-wrap justify-content-center">
+        <div class="d-flex flex-row">
+            <img src="istock.jpg" class="img-fluid" alt="connect to internet to view the image">
+        </div>
+        
+    </div>
+</div>
+
+@stop
